5d04bc97161a504ac85383ef3d2d1ca621b89c0e
Luis Mora <moraluis1325@gmail.com>
2023-04-16 17:01:24 -0700

    Camera Classes, Notification Update, CSV file
    Merged Top-View and Side View into single class. The constructor takes in file names and camera index to make new object.
    Made Global variables to update notification banner
    Moved CSV into global variable and will write when notification updates.

--- main.py
+++ main.py
@@ -17,15 +17,13 @@ class MainWindow(QMainWindow):
         uic.loadUi("can_detection_gui.ui", self)
         self.show()
 
-        """ Create Camera Feeds and Connect Labels"""
-        self.top_camera = TopCameraFeedTinyYOLOV4()
+        """ Create Camera Feeds and Connect Labels """
+        self.top_camera = CameraFeed('top-detection_best.weights', 'top-detection.cfg', 'top-detection.names', 1, 'top')
         self.top_camera.image_update.connect(self.image_update_slot1)
         self.top_camera.notification_update.connect(self.notification_banner_update)
-        self.left_camera = CameraFeed()
+        self.left_camera = CameraFeed('sideview-yolov4-tiny-detector_best.weights', 'sideview-yolov4-tiny-detector.cfg', 'obj.names', 0, 'left')
         self.left_camera.image_update.connect(self.image_update_slot2)
-        #self.left_camera.notification_update.connect(self.notification_banner_update)
-        #self.right_camera = CameraFeed()
-        #self.right_camera.image_update.connect(self.image_update_slot)
+        self.left_camera.notification_update.connect(self.notification_banner_update)
 
         """ Connect Power Button """
         self.power = False
@@ -33,9 +31,7 @@ class MainWindow(QMainWindow):
         self.on_pushButton.setFlat(True)
         self.on_pushButton.clicked.connect(self.turn_on)
 
-    """ Connect Camera to Label """
-    # Connect Camera to Labelca
-        # Connect Camera to Labelca
+    """ Connect CameraFeed Signal to Label """
     def image_update_slot1(self, image):
         self.camera1.setPixmap(QPixmap.fromImage(image))
 
@@ -46,140 +42,112 @@ class MainWindow(QMainWindow):
     #    self.camera3.setPixmap(QPixmap.fromImage(image))
         
     """ Update Notification Banner """
-    def notification_banner_update(self, acceptance):
-        if acceptance:
+    def notification_banner_update(self):
+        # Global variables for CSV file
+        global csv_file, csv_writer
+        # Global variables for can classification
+        global set_emit1, set_emit2
+
+        # Compare and update banner
+        acceptance = ''
+        if set_emit1 and set_emit2:
             self.notification_label.setStyleSheet(open('accepted.css').read())
-            self.notification_label.setText("ACCEPTED")
+            self.notification_label.setText('ACCEPTED')
+            acceptance = 'GOOD'
         else:
             self.notification_label.setStyleSheet(open('rejected.css').read())
-            self.notification_label.setText("REJECTED")
+            self.notification_label.setText('REJECTED')
+            acceptance = 'DAMAGED'
+        
+        # Write rejection/acceptance to CSV file
+        current_time = datetime.datetime.now()
+        csv_writer.writerow([acceptance, current_time])
 
     def startup(self):
-        self.on_pushButton.setStyleSheet("QPushButton { background-image: url(off.png); }")
-        self.logo_label.setStyleSheet("QLabel { image: url(NJFCO_logo.png);}")
+        self.on_pushButton.setStyleSheet('QPushButton { background-image: url(off.png); }')
+        self.logo_label.setStyleSheet('QLabel { image: url(NJFCO_logo.png);}')
 
     """ On/Off button click """
     def turn_on(self):
         if self.power:
-            self.on_pushButton.setStyleSheet("QPushButton { background-image: url(off.png); }")
+            self.on_pushButton.setStyleSheet('QPushButton { background-image: url(off.png); }')
             self.notification_label.setStyleSheet(open('rejected.css').read())
-            self.notification_label.setText("OFF")
+            self.notification_label.setText('OFF')
             self.top_camera.stop()
             self.left_camera.stop()
             #self.right_camera.stop()
             self.power = False
         else:
-            self.on_pushButton.setStyleSheet("QPushButton { background-image: url(on.png); }")
+            self.on_pushButton.setStyleSheet('QPushButton { background-image: url(on.png); }')
             self.notification_label.setStyleSheet(open('accepted.css').read())
-            self.notification_label.setText("ON")
+            self.notification_label.setText('ON')
             self.top_camera.start()
             self.left_camera.start()
             #self.right_camera.start()
             self.power = True
 
-class TopCameraFeedTinyYOLOV4(QThread):
+""" Camera Feed Thread """
+class CameraFeed(QThread):
     image_update = pyqtSignal(QImage)
-    notification_update = pyqtSignal(bool)
+    notification_update = pyqtSignal()
 
+    def __init__(self, weights, config, labels, cam_index, pos) -> None:
+        super().__init__()
+        self.weights = weights
+        self.config = config
+        self.labels = labels
+        self.capture = cv2.VideoCapture(cam_index)
+        self.cam_position = pos
+    
     def run(self):
         self.ThreadActive = True
-
-        cap = cv2.VideoCapture(0)
-        weights = "top-detection_best.weights"
-        config = "top-detection.cfg"
-        labels = "top-detection.names"
-        color = (255,255,255)
-        count = 0
-
-        csv_file = open('object_detection.csv', 'w')
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['Object', 'Confidence', 'Time' ])
-
+        color = (255,255,255)   
 
         while self.ThreadActive:
-            ret,img=cap.read()
+            ret, frame = self.capture.read()
             #Lower framerate
             #count += 1
             #if count % 10 != 0:
             #    continue
-            img=cv2.resize(img,(400,250))
-            
-            #Trained Model
-            yolo = YOLO(weights, config, labels)
-            
-            #outputs boundary, label, and confidence of can when detected
-            bbox, label, conf = yolo.detect_objects(img)
-            
-            for i in range(len(label)):
-                if label[i] == 'Damaged Can' or label[i] == 'Good can':
-                    if conf[i] > .98:
-                        current_time = datetime.datetime.now()
-                        csv_writer.writerow([label[i], conf[i], current_time])
-            
-            #draws boundary box at the location of can
-            cam = yolo.draw_bbox(img, bbox, label, conf, color)
-            
-            try:
-                # In list of detected objects identify the most confident
-                can = conf.index(max(conf))
 
-                # Emit to notification banner
-                if label[can] == 'Good can':
-                    self.notification_update.emit(True)
-                else:
-                    self.notification_update.emit(False)
-            except ValueError:
-                print("No object detected")
-            
-            convert_to_QtFormat = QImage(img.data, img.shape[1], img.shape[0], QImage.Format_RGB888)
-            pic = convert_to_QtFormat.scaled(400, 250, Qt.KeepAspectRatio)
-            self.image_update.emit(pic)
-
-
-    def stop(self):
-        self.ThreadActive = False
-        self.quit()
-
-""" Camera Feeds Threads """
-class CameraFeed(QThread):
-    image_update = pyqtSignal(QImage)
-    notification_update = pyqtSignal(bool)
-    
-    def run(self):
-        self.ThreadActive = True
-        weights = "sideview-yolov4-tiny-detector_best.weights"
-        config = "sideview-yolov4-tiny-detector.cfg"
-        labels = "obj.names"
-        color = (100,100,100)   
-        capture = cv2.VideoCapture(1)
-
-        while self.ThreadActive:
-            ret, frame = capture.read()
-            
+            """ Call Object Detection on Image """
             if ret:
-                img = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), (320, 320))
-                yolo = YOLO(weights, config, labels)
+                # Resize image and detect object
+                img = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), (400, 250))
+                yolo = YOLO(self.weights, self.config, self.labels)
                 bbox, label, conf = yolo.detect_objects(img)
                 cam = yolo.draw_bbox(img, bbox, label, conf, color)
-                print(label)
-                print(conf)
 
-                # try:
-                #     # In list of detected objects identify the most confident
-                #     can = conf.index(max(conf))
+                """ Classify Detected Object and Update Notification"""
+                try:
+                    # In list of detected objects identify the most confident
+                    can = conf.index(max(conf))
+                    # Make sure confidence level is high enough
+                    if conf[can] < .85:
+                        continue
+                    # Global variable for can classification
+                    global set_emit1, set_emit2
 
-                #     # Emit to notification banner
-                #     if label[can] == 'good':
-                #         self.notification_update.emit(True)
-                #     else:
-                #         self.notification_update.emit(False)
-                # except ValueError:
-                #     print("No object detected")
+                    # Set can global variable
+                    if label[can] == 'Good can' and self.cam_position == 'top':
+                        set_emit1 = True
+                    elif label[can] == 'Damaged Can' and self.cam_position == 'top':
+                        set_emit1 = False
+                        print(label[can])
+                    elif label[can] == 'good' and self.cam_position == 'left':
+                        set_emit2 = True
+                    elif label[can] == 'bad' and self.cam_position == 'left':
+                        set_emit2 = False
+                    
+                    # Update Banner
+                    self.notification_update.emit()
+
+                except ValueError:
+                    print('No object detected')
 
                 """ Recreate QT compatible image"""
-                flipped_img = cv2.flip(img, 1)
-                convert_to_QtFormat = QImage(flipped_img.data, flipped_img.shape[1], flipped_img.shape[0], QImage.Format_RGB888)
-                pic = convert_to_QtFormat.scaled(320, 320, Qt.KeepAspectRatio)
+                convert_to_QtFormat = QImage(img.data, img.shape[1], img.shape[0], QImage.Format_RGB888)
+                pic = convert_to_QtFormat.scaled(400, 250, Qt.KeepAspectRatio)
                 self.image_update.emit(pic)
                 
 
@@ -195,4 +163,9 @@ def main():
     app.exec()
 
 if __name__ == '__main__':
+    set_emit1 = False
+    set_emit2 = False
+    csv_file = open('object_detection.csv', 'w')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Object', 'Time' ])
     main()
